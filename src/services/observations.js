e684ab023027d70907a67f1940ceb6d515daa9e3
Elisa Nurmi <nurmi.elisa@gmail.com>
2019-10-01 21:09:05 +0300

    read api url from env

--- src/services/observations.js
+++ src/services/observations.js
@@ -1,18 +1,18 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3004/observations'
+const baseUrl = process.env.API_URL || 'http://localhost:3004/api'
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
+  const request = axios.get(baseUrl + '/observations')
   return request.then(response => response.data)
 }
 
 const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
+  const request = axios.post(baseUrl + '/observations', newObject)
   return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  const request = axios.put(`${baseUrl}/observations/${id}`, newObject)
   return request.then(response => response.data)
 }
 
