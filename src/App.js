f786bd845b22e06a051af94ecad312e020837435
Elisa Nurmi <nurmi.elisa@gmail.com>
2019-10-03 19:58:30 +0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,72 +3,22 @@ import logo from './vladimir-kudinov-orng-mDXPnk-unsplash.jpg';
 import './App.css';
 import observationService from './services/observations';
 
-/* import Birds from './components/Birds';
-import Bird from './components/Bird'
-import Button from './components/Button'
-import styled, { css } from 'styled-components'
-import ButtonDetails from './components/ButtonDetails'
- */
-
-/* const ButtonNew = ({ handleNew }) => {
-  console.log('...ButtonNew')
-  return (
-    <div>
-      <Button
-        onClick={handleNew}>
-        new observation
-      </Button>
-    </div>
-  )
-} */
-
-/* const NewObservation = () => {
-  console.log('....NewObservation')
-  return (
-    <div>
-      <form>
-        <table>
-          <tr>
-            <label>
-              Name:
-              <input type="text" name="name" />
-            </label>
-          </tr>
-          <tr>
-            <textarea name="note" rows="10" cols="30">
-              Observation notes
-            </textarea>
-          </tr>
-          <tr>
-            <select>
-              <option selected value="common">Common</option>
-              <option value="rare">Rare</option>
-              <option value="extremely rare">Extremely rare</option>
-            </select>
-          </tr>
-          <tr>
-            <input type="submit" value="Submit" />
-          </tr>
-        </table>
-      </form>
-    </div>
-  )
-} */
-
 const Observation = ({ observation }) => {
   return (
-    <li>{observation.name}</li>
+    <tr>
+      <td>{observation.datetime}</td>
+      <td>{observation.name}</td>
+      <td>{observation.scientificName}</td>
+      <td>{observation.rarity}</td>
+    </tr>
   )
 }
 
 function App() {
 
-  /* const [showBird, setShowBird] = useState(null)
- */
   const [observations, setObservations] = useState([])
-  const [newObservation, setNewObservation] = useState([])
   const [newName, setNewName] = useState('')
-  const [newRarity, setNewRarity] = useState('')
+  const [newRarity, setNewRarity] = useState('common')
   const [newScientificName, setNewScientificName] = useState('')
 
   // fetch all observations from API
@@ -96,23 +46,20 @@ function App() {
       .then(returnedObject => {
         console.log('returned object...', returnedObject)
         setObservations(observations.concat(returnedObject))
-        setNewObservation('')
         setNewName('')
         setNewScientificName('')
-        setNewRarity('')
+        setNewRarity('common')
       })
   }
 
   const observationRows = () => observations.map(observation =>
-    <Observation
-      key={observation.id}
-      observation={observation}
-    />
+    <table>
+      <Observation
+        key={observation.id}
+        observation={observation}
+      />
+    </table>
   )
-
-  const handeleNewObservationChange = (event) => {
-    setNewObservation(event.target.value)
-  }
 
   const handeleNewNameChange = (event) => {
     setNewName(event.target.value)
@@ -137,7 +84,7 @@ function App() {
       <p></p>
 
       <div>
-        <ul>{observationRows()}</ul>
+        <div>{observationRows()}</div>
         <form onSubmit={addObservation}>
           <label>Name
           <input
@@ -154,16 +101,13 @@ function App() {
           </label>
           <br />
           <label>Rarity
-            {/* <select value={newRarity} onChange={handeleNewRarityChange}>
-              <option value="common">Common</option>
-              <option value="rare">Rare</option>
-              <option value="extremely rare">Extremely rare</option>
-            </select> */}
-            <input
-              value={newRarity}
-              onChange={handeleNewRarityChange}
-            />
           </label>
+
+          <select value={newRarity} onChange={handeleNewRarityChange}>
+            <option value="common">Common</option>
+            <option value="rare">Rare</option>
+            <option value="extremely rare">Extremely rare</option>
+          </select>
 
 
           <button type="submit">save</button>
